cea6424c0d76e00ea9e2a448a298044c48e9564b
pepexqw <superampleev@gmail.com>
2026-01-08 15:08:30 +0300

    Добавлен новый пользователь: 164.92.117.50

--- registers.js
+++ registers.js
@@ -5,5 +5,12 @@ const registers = [
     "first_seen": "2026-01-08T12:08:29.430946",
     "last_active": "2026-01-08T12:08:29.430960",
     "uploads_count": 0
+  },
+  {
+    "id": 2,
+    "ip": "164.92.117.50",
+    "first_seen": "2026-01-08T12:08:30.425397",
+    "last_active": "2026-01-08T12:08:30.425410",
+    "uploads_count": 0
   }
 ];
